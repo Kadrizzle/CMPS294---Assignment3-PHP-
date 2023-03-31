9b01890a90496975e995487fb4b380899d3bbd85
Kadrizzle <98795639+Kadrizzle@users.noreply.github.com>
2023-03-31 15:08:38 -0500

    Add files via upload

---
+++ viewOfDataBase.php
@@ -0,0 +1,25 @@
+<!DOCTYPE
+
+<?php
+
+//In this file I will be displaying the contents of the library to the user
+//As im looping im going to have use a format to have a header for the title, author, etc...
+
+    $dataBase = fopen('data.txt', 'r');
+
+    while(!feof($dataBase)){
+        explode("%", $dataBase);
+        
+        <table>
+
+        <tr>
+        <td>
+
+        </td>
+        </tr>
+
+
+        </table>
+    }
+
+?>
